071897c47bb1df47f5b4c41ad920db135449a5be
Utkarshni gaur <119881393+utkarshni@users.noreply.github.com>
2026-01-07 15:36:44 +0530

    Add files via upload

---
+++ day9/Exception.cs
@@ -0,0 +1,20 @@
+﻿namespace LPU_Exception
+{
+    /// <summary>
+    ///  Custom Exception class Created for Lpu Project 
+    ///  by Harshita Bajpai on Date 29/12/25 at 11:34 AM
+    /// </summary>
+    public class LpuException : Exception
+    {
+        public LpuException() : base()
+        {
+
+        }
+        public LpuException(string message) : base(message)
+        {
+
+        }
+
+
+    }
+}
---
+++ day9/LPUEntity/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace LPU_Entity
+{
+
+
+    //Enums ca be defined can be defined at namespcae level
+    //Sudent can choose only these courses
+    public enum CourseType
+    {
+        Mechanical = 10,//by default it is =0 so if we don't provide mechanical = 10 n just write mechinical it will be 0
+        Electrical = 20,//this is =1
+        Civil = 30,// this is =2
+        CSE = 40,
+        IT = 50
+    }
+    public class Student
+    {
+        //auto inplicit properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+
+        //Property of type enum
+        public CourseType Course { get; set; }
+
+    }
+}
