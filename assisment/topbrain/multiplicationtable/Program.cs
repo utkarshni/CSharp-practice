ccfa6358ba772765679b47f44d8a33cc4b5fe0e8
Utkarshni gaur <119881393+utkarshni@users.noreply.github.com>
2026-01-19 14:43:54 +0530

    Add files via upload

---
+++ assisment/topbrain/multiplicationtable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter upto value: ");
+        int upto = int.Parse(Console.ReadLine());
+
+        int[] result = GetMultiplicationRow(n, upto);
+
+        Console.WriteLine("Multiplication row:");
+        Console.WriteLine(string.Join(", ", result));
+    }
+
+    static int[] GetMultiplicationRow(int n, int upto)
+    {
+        if (upto <= 0)
+            return new int[0];
+
+        int[] row = new int[upto];
+
+        for (int i = 1; i <= upto; i++)
+        {
+            row[i - 1] = n * i;
+        }
+
+        return row;
+    }
+}
+
